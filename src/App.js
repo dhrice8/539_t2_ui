51087d9cd181800978a9ffa55db9dce050015926
Lisachen0208 <lisa@nixbiosensors.com>
2023-10-09 01:19:37 -0500

    first demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [longUrl, setLongUrl] = useState('');
+  const [shortUrl, setShortUrl] = useState('');
+
+  const handleSubmit = async () => {
+    
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container mt-5">
+      <h1 className="text-center mb-4">URL Shortener</h1>
+      <div className="input-group mb-3">
+        <input 
+          type="url" 
+          className="form-control"
+          placeholder="Enter long URL" 
+          value={longUrl}
+          onChange={(e) => setLongUrl(e.target.value)}
+        />
+        <div className="input-group-append">
+          <button className="btn btn-primary" onClick={handleSubmit}>Shorten</button>
+        </div>
+      </div>
+      {shortUrl && <div className="text-center mt-3">Short URL: <a href={shortUrl}>{shortUrl}</a></div>}
     </div>
   );
 }
